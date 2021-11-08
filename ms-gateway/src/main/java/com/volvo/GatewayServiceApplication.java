46baa89e13e005fc7f0e89887e0b06578e13117a
RAJA MARAGANI <raja.maragani@ericsson.com>
2021-11-08 21:45:22 +0100

    Test cases implemented

--- ms-gateway/src/main/java/com/volvo/GatewayServiceApplication.java
+++ ms-gateway/src/main/java/com/volvo/GatewayServiceApplication.java
@@ -2,8 +2,10 @@ package com.volvo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.volvo")
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
--- ms-gateway/src/test/java/com/volvo/controller/GatewayControllerTest.java
+++ ms-gateway/src/test/java/com/volvo/controller/GatewayControllerTest.java
@@ -1,39 +1,58 @@
 package com.volvo.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.ResponseEntity;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(GatewayController.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class GatewayControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-    
-    @Test
-    public void testSendMailSuccess() throws Exception {
+    // bind the above RANDOM_PORT
+    @LocalServerPort
+    private int port;
 
-        /*This test scenario for test the /send-email/post rest endpoint with the positive parameters  */
-        final RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api")
-                .accept(MediaType.ALL)
-                .content("{ helloWorldSA { greetings } }");
-        final MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        final String expected = "Validation error of type FieldUndefined";
-        final MockHttpServletResponse response = result.getResponse();
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
-        assertTrue(response.getContentAsString().contains(expected));
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testSAdown() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<>("{ helloWorldSA { greetings } }", headers);
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(new URL("http://localhost:" + port + "/api").toString(), request, String.class);
+        assertEquals("Service Down. Please try after some time", response.getBody());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
+
+    @Test
+    public void testSBdown() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<>("{ helloWorldSB { greetings } }", headers);
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(new URL("http://localhost:" + port + "/api").toString(), request, String.class);
+        assertEquals("Service Down. Please try after some time", response.getBody());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
+    public void testSCBadRequest() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> request = new HttpEntity<>("{ helloWorldSC { greetings } }", headers);
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(new URL("http://localhost:" + port + "/api").toString(), request, String.class);
+        assertEquals("Bad Service Request.", response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
 }
